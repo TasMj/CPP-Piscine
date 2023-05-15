920181f78bde185b90aee2d28ddd4f9915aef97f
TasMj <tasnim.mej@hotmail.com>
2023-05-15 10:23:15 +0200

    set up

---
+++ cpp00/ex02/Account.cpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tas <tas@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/15 10:04:34 by tas               #+#    #+#             */
+/*   Updated: 2023/05/15 10:22:22 by tas              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+int Account::getNbAccounts( void )
+{
+    
+}
+
+int Account::getTotalAmount( void )
+{
+    
+}
+
+int Account::getNbDeposits( void )
+{
+    
+}
+
+int Account::getNbWithdrawals( void )
+{
+    
+}
+
+int Account::displayAccountsInfos( void )
+{
+    
+}
+
+Account::Account( int initial_deposite )
+{
+    
+}
+
+Account::~Account( void )
+{
+    
+}
+
+void    Account::makeDeposit( int deposite )
+{
+    
+}
+
+bool Account::makeWithdrawal( int withdrawal )
+{
+    
+}
+
+int Account::checkAmount( void ) const
+{
+    
+}
+
+void    Account::displayStatus( void ) const
+{
+    
+}
